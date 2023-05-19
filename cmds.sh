5d426c657c4194e87e70f80e84c91e7367b893b4
Givi Tsvariani <thommaster10@gmail.com>
2023-05-19 00:37:42 -0400

    Update cmds.sh

--- cmds.sh
+++ cmds.sh
@@ -1,3 +1,55 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo snap refresh
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo npm install -g typescript
+sudo apt install python3-pip
+sudo apt install snapd
+sudo snap install hello-world
+sudo snap install krita
+sudo snap install spotify
+sudo snap install postman
+sudo snap install inkscape
+sudo snap install kdenlive
+sudo snap install obs-studio
+sudo snap install gitkraken --classic
+sudo snap install foobar2000
+sudo snap install gimp
+sudo snap install skype
+sudo snap install audacity
+sudo snap install bitwarden
+sudo snap install vlc
+sudo snap install eclipse --classic
+sudo snap install kubectl --classic
+sudo snap install insomnia
+sudo snap install go --classic
+sudo snap install code --classic
+sudo snap install ruby --classic
+sudo snap install termius-app
+sudo snap install sublime-text --classic
+sudo snap install notepad-plus-plus
+sudo snap install discord
+sudo snap install brave
+sudo snap refresh
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install wget
+sudo apt install python3-pip
+sudo apt install git
+wget https://github.com/smicallef/spiderfoot/archive/v4.0.tar.gz
+tar zxvf v4.0.tar.gz
+cd spiderfoot-4.0
+pip3 install -r requirements.txt
+cd
+sudo apt install pidgin
+sudo snap refresh
+sudo snap install aliasman
+sudo snap install atom --classic
+sudo snap install opera
+sudo snap install tor-middle-relay
+sudo snap install tor-snowflake
+sudo snap install mattermost-desktop
+sudo snap refresh
+sudo apt-get update
+sudo apt-get upgrade
