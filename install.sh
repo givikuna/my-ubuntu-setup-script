bae3007bc6bfd8b1d11a05a548589788b42fca6d
Givi Tsvariani <thommaster10@gmail.com>
2023-06-08 21:09:10 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,85 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt install snapd
+sudo snap refresh
+
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo npm install -g typescript
+sudo npm install -g coffeescript
+sudo npm install -g livescript
+sudo snap install crystal --classic
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
+sudo apt update
+sudo apt install build-essential
+sudo apt install docker
+sudo apt install git
+sudo apt-get install gitk
+sudo npm install -g elm
+sudo npm install -g purescript
+sudo npm install -g spago
+sudo apt install python3-pip
+sudo snap install ruby --classic
+sudo snap install go --classic
+sudo apt install default-jre
+sudo apt install perl
+sudo apt install php
+sudo apt install lua5.3
+sudo apt-get install julia
+
+sudo snap install snapcraft --classic
+sudo apt install flatpak
+sudo snap install spotify
+sudo snap install gitkraken --classic
+sudo snap install gimp
+sudo snap install vlc
+sudo snap install eclipse --classic
+sudo snap install code --classic
+sudo snap install sublime-text --classic
+sudo snap install notepad-plus-plus
+sudo snap install discord
+sudo snap install brave
+sudo snap install aliasman
+sudo snap install atom --classic
+sudo snap install opera
+sudo snap install tor-middle-relay
+sudo snap install tor-snowflake
+sudo apt install pidgin
+sudo snap install code-insiders --classic
+sudo snap install aliasman
+sudo apt install gnome-tweaks
+sudo snap install arduino
+# sudo flatpak install flathub io.gitlab.adhami3310.Converter
+# sudo flatpak install flathub io.gitlab.theevilskeleton.Upscaler
+# sudo flatpak install flathub org.kde.kolourpaint
+# sudo flatpak install flathub net.codeindustry.MasterPDFEditor
+# sudo flatpak install flathub org.gnome.Evince
+# sudo flatpak install flathub com.valvesoftware.Steam
+# sudo flatpak install flathub io.gitlab.librewolf-community
+# sudo flatpak install flathub org.filezillaproject.Filezilla
+# sudo flatpak install flathub com.github.sdv43.whaler
+# sudo flatpak install flathub org.gnome.meld
+# sudo flatpak install flathub io.qt.QtCreator
+# sudo flatpak install flathub org.ghidra_sre.Ghidra
+# sudo flatpak install flathub io.github.cleomenezesjr.Escambo
+# sudo flatpak install flathub dev.lapce.lapce
+# sudo flatpak install flathub io.qt.Designer
+
+sudo apt install wget
+sudo apt install cloc
+sudo apt install neofetch
+sudo apt install htop
+wget https://github.com/smicallef/spiderfoot/archive/v4.0.tar.gz
+tar zxvf v4.0.tar.gz
+cd spiderfoot-4.0
+pip3 install -r requirements.txt
+cd
+
+sudo apt update
+sudo apt upgrade
+sudo snap refresh
+sudo flatpak update
