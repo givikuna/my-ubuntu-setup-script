722099340e3a83b8a8ca58c469f3f600a96d53c5
Givi Tsvariani <thommaster10@gmail.com>
2023-06-08 20:42:02 -0400

    Delete buildbuntu.py

--- buildbuntu.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-sh_file_path = "cmds.sh"
-with open(sh_file_path, "r") as file:
-    commands = file.read().splitlines()
-for command in commands:
-    command = command.strip()
-    subprocess.run(command.split(), check=True)
