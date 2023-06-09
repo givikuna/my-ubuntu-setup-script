17ab7f01416ec626e9ec55c5d0067781c0caa816
Givi Tsvariani <thommaster10@gmail.com>
2023-06-08 23:08:40 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,14 +29,18 @@ sudo apt install default-jre
 sudo apt install perl
 sudo apt install php
 sudo apt install lua5.3
-sudo apt-get install julia
+sudo snap install julia --classic
+sudo snap install gradle --classic
 
+sudo snap install obs-studio
+sudo snap install android-studio --classic
 sudo snap install snapcraft --classic
 sudo apt install flatpak
 sudo snap install spotify
 sudo snap install gitkraken --classic
 sudo snap install gimp
 sudo snap install vlc
+sudo snap install marktext
 sudo snap install eclipse --classic
 sudo snap install code --classic
 sudo snap install sublime-text --classic
@@ -49,25 +53,38 @@ sudo snap install opera
 sudo snap install tor-middle-relay
 sudo snap install tor-snowflake
 sudo apt install pidgin
+sudo snap install signal-desktop
+sudo snap install jenkins --classic
+sudo snap install sublime-merge --classic
+sudo snap install ddgr
+sudo snap install simplenote
 sudo snap install code-insiders --classic
 sudo snap install aliasman
 sudo apt install gnome-tweaks
 sudo snap install arduino
-# sudo flatpak install flathub io.gitlab.adhami3310.Converter
-# sudo flatpak install flathub io.gitlab.theevilskeleton.Upscaler
-# sudo flatpak install flathub org.kde.kolourpaint
-# sudo flatpak install flathub net.codeindustry.MasterPDFEditor
-# sudo flatpak install flathub org.gnome.Evince
-# sudo flatpak install flathub com.valvesoftware.Steam
-# sudo flatpak install flathub io.gitlab.librewolf-community
-# sudo flatpak install flathub org.filezillaproject.Filezilla
-# sudo flatpak install flathub com.github.sdv43.whaler
-# sudo flatpak install flathub org.gnome.meld
-# sudo flatpak install flathub io.qt.QtCreator
-# sudo flatpak install flathub org.ghidra_sre.Ghidra
-# sudo flatpak install flathub io.github.cleomenezesjr.Escambo
-# sudo flatpak install flathub dev.lapce.lapce
-# sudo flatpak install flathub io.qt.Designer
+sudo flatpak install flathub io.gitlab.adhami3310.Converter
+sudo flatpak install flathub io.gitlab.theevilskeleton.Upscaler
+sudo flatpak install flathub org.kde.kolourpaint
+sudo flatpak install flathub net.codeindustry.MasterPDFEditor
+sudo flatpak install flathub org.gnome.Evince
+sudo flatpak install flathub com.valvesoftware.Steam
+sudo flatpak install flathub io.gitlab.librewolf-community
+sudo flatpak install flathub org.filezillaproject.Filezilla
+sudo flatpak install flathub com.github.sdv43.whaler
+sudo flatpak install flathub org.gnome.meld
+sudo flatpak install flathub io.qt.QtCreator
+sudo flatpak install flathub org.ghidra_sre.Ghidra
+sudo flatpak install flathub io.github.cleomenezesjr.Escambo
+sudo flatpak install flathub dev.lapce.lapce
+sudo flatpak install flathub io.qt.Designer
+sudo snap install chromium
+sudo flatpak install flathub org.srb2.SRB2
+sudo flatpak install flathub com.github.Murmele.Gittyup
+sudo flatpak install flathub org.jupyter.JupyterLab
+sudo flatpak install flathub org.gnome.Boxes
+sudo flatpak install flathub fr.romainvigier.MetadataCleaner
+sudo flatpak install flathub com.github.unrud.VideoDownloader
+sudo flatpak install flathub org.qelectrotech.QElectroTech
 
 sudo apt install wget
 sudo apt install cloc
